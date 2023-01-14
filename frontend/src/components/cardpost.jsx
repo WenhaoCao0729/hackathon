de18f59445fde824d1b3c194229bebd920e2b17e
filippociandy10 <filippo.ciandy10@gmail.com>
2023-01-14 13:06:05 -0700

    Fix card name issues and app

--- frontend/src/components/cardpost.jsx
+++ frontend/src/components/cardpost.jsx
@@ -1,33 +1,45 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import ToggleButton from '@mui/material/ToggleButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Container from '@mui/material/Container';
 
-const CardPost = () =>{
+export default function CardPost({postObj}) {
+    const [selected, setSelected] = React.useState(false);
+    const [likes, setLikes] = React.useState(0)
     return (
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 140 }}
-            image="/static/mykonos"
-            title="Mykonos"
+            image= {postObj.imageUrl}
+            title= {postObj.title}
           />
           <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+            {postObj.title}
+            </Typography>
             <Typography gutterBottom variant="h5" component="div">
-              Lizard
+              {postObj.location}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-            Mykonos has been known as the island of the winds, 
-            and it's located in the heart of the Cyclades group. 
-            Its fame has spread across the world, as a cosmopolitan and luxurious holiday destination.
+            {postObj.content}
             </Typography>
+            <Container>
+              <ToggleButton
+                selected={selected}
+                onChange={() => {
+                  setSelected(!selected);
+                  setLikes( !selected ? (likes => likes + 1) : (likes => likes - 1));
+                }}
+              >
+                <FavoriteIcon/>
+              </ToggleButton>
+              <Typography> {likes} </Typography>
+            </Container>
           </CardContent>
-          {/* <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions> */}
         </Card>
       );
 }   
