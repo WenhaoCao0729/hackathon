08c921d5b9a093cc51ee15426633f35cf89498eb
John Goodliff <johny112009@hotmail.com>
2023-01-14 18:32:01 -0700

    Add validation for text fields in new post dialog

--- frontend/src/components/new-post-popup.jsx
+++ frontend/src/components/new-post-popup.jsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputAdornment, Stack, TextField } from '@mui/material';
+import { Alert, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputAdornment, Stack, TextField } from '@mui/material';
 import { Add, Send, PhotoCamera, LocationOn } from '@mui/icons-material';
 
 
+const MAX_FIELD_LENGTH = 64;
+
 const NewPostDialog = (props) => {
   const [open, setOpen] = React.useState(false);
+  const [postTitle, setPostTitle] = React.useState('');
+  const [postLocation, setPostLocation] = React.useState('');
+  const [postDesc, setPostDesc] = React.useState('');
+
+  const validateField = (value, successCallback, maxFieldLength = MAX_FIELD_LENGTH) => {
+    if (value.length > maxFieldLength) {
+      return;
+    }
+
+    successCallback(value);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -15,8 +28,24 @@ const NewPostDialog = (props) => {
   };
 
   const handleSubmit = () => {
-    handleClose();
-    // TODO: Call endpoint to add post to database
+    // TODO: Check if image has been uploaded here. This line exists in other places too
+    if (postTitle && postLocation && postDesc) {
+      handleClose();
+      // TODO: Call endpoint to add post to database
+    }
+  };
+
+  const handleTitleChange = (event) => {
+    validateField(event.target.value, setPostTitle);
+  };
+
+  const handleLocationChange = (event) => {
+    validateField(event.target.value, setPostLocation);
+  };
+
+
+  const handleDescChange = (event) => {
+    validateField(event.target.value, setPostDesc, 256);
   };
 
   return (
@@ -38,22 +67,18 @@ const NewPostDialog = (props) => {
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             <Stack spacing={3}>
-              <div></div>
-              {/* <TextField id="outlined-basic" label="Username" variant="outlined" /> */}
-              <TextField id="outlined-basic" label="Title" variant="outlined" />
-              <TextField id="outlined-basic" label="Location" variant="outlined" InputProps={{
+              <TextField id="post-title-field" label="Title" variant="outlined" margin="dense" value={postTitle} onChange={handleTitleChange} error={!postTitle} />
+              <TextField id="post-location-field" label="Location" variant="outlined" value={postLocation} onChange={handleLocationChange} error={!postLocation} InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
                     <LocationOn />
                   </InputAdornment>
                 ),
               }} />
-              <TextField id="outlined-basic" label="Description" variant="outlined" multiline rows={4} />
-
-              {/* TODO: Add component that lets How to upload images here */}
+              <TextField id="post-desc-field" label="Description" variant="outlined" multiline rows={4} value={postDesc} onChange={handleDescChange} error={!postDesc} />
               <Button variant="contained" component="label" startIcon={<PhotoCamera />}>
                 Upload Image
-                <input hidden accept="image/*" multiple type="file" />
+                <input id="post-image-field" hidden accept="image/*" multiple type="file" />
               </Button>
             </Stack>
           </DialogContentText>
@@ -62,10 +87,11 @@ const NewPostDialog = (props) => {
           <Button onClick={handleClose}>
             Cancel
           </Button>
-          <Button onClick={handleSubmit} startIcon={<Send />} autoFocus>
+          <Button onClick={handleSubmit} startIcon={<Send />} autoFocus disabled={!(postTitle && postLocation && postDesc)}>
             Post
           </Button>
         </DialogActions>
+        {!(postTitle && postLocation && postDesc) && <Alert severity="error">Oof! All fields are required</Alert>}
       </Dialog>
     </div>
   );
